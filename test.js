3b3b7aa466e4f1cf01bd6ca38d6736e0e5cf017e
xseignard <xavier.seignard@gmail.com>
2015-04-02 17:45:10 +0200

    post btn push to server

--- test.js
+++ test.js
@@ -1,14 +1,16 @@
 var request = require('request');
-var url = 'http://dashboard.sidlee.com/api/1/event/today';
 
-request(url, function (error, response, body) {
-	if (!error && response.statusCode == 200) {
-		var values = {};
+var postPush = function() {
+	var url = 'http://dashboard.sidlee.com/api/1/event';
 
-		JSON.parse(body).map(function(current) {
-			var id = current._id;
-			values[id] = current.value;
-		});
-		console.log(values);
-	}
-});
+	var event = {
+		name: 'lightswitch',
+		value: 1,
+		unit: 'click',
+		token: 'c7f2ad85-a221-6fbf-22e2-9bcca6994c75'
+	};
+
+	request.post({ method: "POST", url: url, json: true, body: event });
+};
+
+postPush();
